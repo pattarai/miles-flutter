e0b6a9d3480b810748b1d62db44d55081a57e0ca
Rahuls-iMac <rahwinside@gmail.com>
2021-04-08 01:46:13 +0530

    Finalise Scan QR design

--- lib/screens/testWidget.dart
+++ lib/screens/testWidget.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:miles/helper/styles.dart';
+import 'package:miles/screens/lander.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 
 void main() => runApp(MaterialApp(home: TestWidget()));
@@ -39,7 +40,10 @@ class _TestWidgetState extends State<TestWidget> {
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text("Scan QR", style: headerStyle,),
+                Text(
+                  "Scan QR",
+                  style: headerStyle,
+                ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.center,
@@ -52,49 +56,45 @@ class _TestWidgetState extends State<TestWidget> {
                         child: FutureBuilder(
                           future: controller?.getFlashStatus(),
                           builder: (context, snapshot) {
-                            return snapshot.data == true? Icon(Icons.flash_on): Icon(Icons.flash_off);
+                            return snapshot.data == true
+                                ? Icon(
+                                    Icons.flash_on,
+                                    size: 40,
+                                  )
+                                : Icon(
+                                    Icons.flash_off,
+                                    size: 40,
+                                  );
                           },
                         )),
+                    Padding(
+                      padding: EdgeInsets.only(right: 8),
+                    ),
                     InkWell(
                         onTap: () async {
-                    await controller?.flipCamera();
-                    setState(() {});
-                    },
+                          await controller?.flipCamera();
+                          setState(() {});
+                        },
                         child: FutureBuilder(
                           future: controller?.getFlashStatus(),
                           builder: (context, snapshot) {
                             if (snapshot.data != null) {
-                              return Icon(Icons.flip_camera_ios);
+                              return Icon(Icons.flip_camera_ios, size: 40);
                             } else {
-                              return Icon(Icons.flip_camera_android_outlined);
+                              return Icon(
+                                Icons.flip_camera_android_outlined,
+                                size: 40,
+                              );
                             }
                           },
                         )),
-
                   ],
                 ),
               ],
             ),
           ),
           Expanded(flex: 4, child: _buildQrView(context)),
-          Expanded(
-            flex: 1,
-            child: FittedBox(
-              fit: BoxFit.contain,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  if (result != null)
-                    Text(
-                        'Barcode Type: ${describeEnum(result!.format)}   Data: ${result!.code}')
-                  else
-                    Text('Scan a code'),
 
-
-                ],
-              ),
-            ),
-          )
         ],
       ),
     );
@@ -103,7 +103,7 @@ class _TestWidgetState extends State<TestWidget> {
   Widget _buildQrView(BuildContext context) {
     // For this example we check how width or tall the device is and change the scanArea and overlay accordingly.
     var scanArea = (MediaQuery.of(context).size.width < 400 ||
-        MediaQuery.of(context).size.height < 400)
+            MediaQuery.of(context).size.height < 400)
         ? 150.0
         : 300.0;
     // To ensure the Scanner view is properly sizes after rotation
@@ -128,6 +128,8 @@ class _TestWidgetState extends State<TestWidget> {
       setState(() {
         result = scanData;
       });
+      // Perform verification here
+      Navigator.push(context, MaterialPageRoute(builder: (context) => Lander()));
     });
   }
 
